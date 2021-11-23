5cda8440dd726ae6f9fd86d8813bbfc0366256d5
rsae0114 <69199490+rsae0114@users.noreply.github.com>
2021-11-23 12:14:25 +0100

    Create account-hibp.php

---
+++ account-hibp.php
@@ -0,0 +1,30 @@
+<?php # -*- coding: utf-8 -*-
+/*
+Plugin Name: HaveIBeenPwned Account
+Author: rsae
+*/
+
+// API url
+$HIBP_BASE_URI = 'https://haveibeenpwned.com/api/v3/breachedaccount/';
+
+$email = 'foo@bar.com';
+$key = 'yourkey';
+
+$encoded = urlencode($email);
+
+$truncateResponse = '?truncateResponse=false';
+
+// Complete url
+$url = $HIBP_BASE_URI . $encoded . $truncateResponse;
+
+$list = array("hibp-api-key: $key", "user-agent: Beyond the Frame");
+
+$client = curl_init();
+curl_setopt($client, CURLOPT_URL, $url);
+curl_setopt($client, CURLOPT_HTTPHEADER, $list);
+curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+$allData = curl_exec($client);
+curl_close($client);
+
+echo $allData;
+?>
